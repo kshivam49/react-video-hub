d28a3b23db445c0ecd083d1fc41147484cfe47be
kshivam49 <kshivam49@gmail.com>
2023-08-13 11:36:34 +0530

    starting the project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+  <Router>
+    <Routes>
+      <Route />
+    </Routes>
+  </Router>
+  )
+}
+
+export default App;
